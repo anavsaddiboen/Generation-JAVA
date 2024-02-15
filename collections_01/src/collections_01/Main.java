34ae64292fef0c815c1ac3023c7b99f708ca013d
anavsaddiboen <anavsaddi@gmail.com>
2024-02-15 15:35:25 -0300

    Atividades Java - Collections

---
+++ collections_01/src/collections_01/Main.java
@@ -0,0 +1,38 @@
+package collections_01;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("JAVA | COLLECTIONS | ATV 01\n");
+		
+		ArrayList <String> cores = new ArrayList<>();
+		
+		for (int i = 0; i<5; i++) {
+			System.out.print("Digite a "+(i+1)+"ª cor:\n");		
+			cores.add(leia.next());
+			
+		}
+		
+
+		System.out.print("Listar todas as cores:\n\n");
+		for (int i = 0; i<5; i++) {
+			System.out.print(cores.get(i)+"\n");
+		}
+
+        Collections.sort(cores);
+
+        System.out.println("\nOrdenar as cores:\n");
+        for (String cor : cores) {
+            System.out.println(cor);
+        }
+
+	}
+
+}
---
+++ collections_03/src/collections_03/Main.java
@@ -0,0 +1,34 @@
+package collections_03;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        
+		System.out.println("JAVA | COLLECTIONS | ATV 03\n");
+
+        System.out.println("Digite 10 valores inteiros:");
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite o " + (i + 1) + "º valor: ");
+            set.add(leia.nextInt());
+        }
+
+        System.out.println("\nListar dados do Set:\n");
+
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
